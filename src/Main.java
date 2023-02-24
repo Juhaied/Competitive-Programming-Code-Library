386b1e84df227f662cb3b17b0bb718b84f12d870
Juhaied <juhaied.hossen@northsouth.edu>
2023-02-24 23:03:55 +0600

    crearting class for check

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int t = in.nextInt();
+        for (int tt = 0; tt < t; tt++) {
+
+
+        }
+        pw.close();
+    }
+
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj));
+    }
+}
