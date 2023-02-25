430b1b9c3565d634c6808f8fe7bd33ce796d52da
Juhaied <juhaied.hossen@northsouth.edu>
2023-02-25 20:44:26 +0600

    Bread First Search

---
+++ src/BFS.java
@@ -0,0 +1,84 @@
+// Java program to print BFS traversal from a given source vertex.
+// BFS(int s) traverses vertices reachable from s.
+import java.io.*;
+import java.util.*;
+
+// This class represents a directed graph using adjacency list
+// representation
+class BFS
+{
+    private int V; // No. of vertices
+    private LinkedList<Integer> adj[]; //Adjacency Lists
+
+    // Constructor
+    BFS(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    // prints BFS traversal from a given source s
+    void BFS(int s)
+    {
+        // Mark all the vertices as not visited(By default
+        // set as false)
+        boolean visited[] = new boolean[V+1];
+
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        // Mark the current node as visited and enqueue it
+        visited[s]=true;
+        queue.add(s);
+
+        while (queue.size() != 0)
+        {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+            System.out.print(s+" ");
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    // Driver method to
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner((System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int ver = in.nextInt()+1;
+        BFS g = new BFS(ver);
+
+        int edge = in.nextInt();
+        for(int i = 0;i<edge;i++) {
+            g.addEdge(in.nextInt(), in.nextInt());
+        }
+        //pw.print("Traversal from vertex 1 : ");
+        g.BFS(1);
+
+        pw.close();
+    }
+}
+
