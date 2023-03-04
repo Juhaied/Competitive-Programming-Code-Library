4852097103a4bb6c74fe14cfc6df6d66c125e096
Juhaied <juhaied.hossen@northsouth.edu>
2023-03-04 11:36:12 +0600

    Graph Represntation 1 Based Indexing

---
+++ Graph_Theory/src/Representation_1Based.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Representation_1Based{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int nodes  = in.nextInt(),edges = in.nextInt();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        //if 1 based indexing
+        for (int i = 0;i<=nodes;i++){
+            adj.add(new ArrayList<>());
+        }
+
+
+        for(int i = 0;i<edges;i++){
+            int u = in.nextInt(), v = in.nextInt();
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        for(int i = 1;i<=nodes;i++){
+            pw.print("Adjacency List of "+i+" : ");
+            for (int j = 0;j<adj.get(i).size();j++){
+                pw.print(adj.get(i).get(j)+" ");
+            }
+            pw.println();
+        }
+
+
+        pw.close();
+    }
+
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj));
+    }
+}
