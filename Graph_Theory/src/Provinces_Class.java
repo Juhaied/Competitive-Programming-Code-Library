8b3b61c04434225058f5f20f65ba10d871a0a6b2
Juhaied <juhaied.hossen@northsouth.edu>
2023-03-07 00:25:30 +0600

    Updated

--- Graph_Theory/src/Provinces_Class.java
+++ Graph_Theory/src/Provinces_Class.java
@@ -2,55 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Provinces_Class {
+    static  boolean vis[];
+    static ArrayList<Integer> []adj;
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
         int n = in.nextInt(),edges = in.nextInt();
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
+        adj =new ArrayList[n+1];
         for(int i = 0;i<=n;i++){
-            adj.add(new ArrayList<>());
+           adj[i] =new ArrayList<>();
         }
 
         for(int i = 0;i<edges;i++){
             int u = in.nextInt(),v = in.nextInt();
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            adj[u].add(v);
+            adj[v].add(u);
         }
-        boolean vis[] = new boolean[n+1];
-        debug(findCircleNum(adj,vis)-1);
+        vis = new boolean[n+1];
+
+        pw.println(findCircleNum()-1);
         pw.close();
     }
 
-    public static int findCircleNum(ArrayList<ArrayList<Integer>>adj,boolean vis[]) {
+    public static int findCircleNum() {
 
         int cnt = 0;
 
-        for(int i = 0;i<adj.size();i++){
+        for(int i = 0;i<vis.length;i++){
             if(vis[i] == false){
                 vis[i] =true;
                 cnt++;
-                dfs(adj,i,vis);
+                dfs(i);
             }
         }
 
         return cnt;
     }
-    static  void dfs(ArrayList<ArrayList<Integer>> adj,int src,boolean[] vis)
-    {
-        int c = 0;
-        vis[src] = true;
-
-        for(int it : adj.get(src))
-        {
-            if(vis[it] == false) {
-                vis[it] = true;
-                dfs(adj, it, vis);
-            }
+    static void dfs(int u) {
+        // debug(u);
+        vis[u] = true;
+        for (int v: adj[u]) {
+            if (!vis[v]) dfs(v);
         }
-
     }
 
     static void debug(Object... obj) {
