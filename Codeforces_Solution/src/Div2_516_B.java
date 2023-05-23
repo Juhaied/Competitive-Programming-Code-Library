5b15469dcf7b50fcd1677d53b5f2f6fced24bfb0
Juhaied <juhaied.hossen@northsouth.edu>
2023-05-23 16:43:37 +0600

    Upsolved

--- Codeforces_Solution/src/Div2_516_B.java
+++ Codeforces_Solution/src/Div2_516_B.java
@@ -1,15 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-public class Div1_516_A {
+public class Div2_516_B {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
-        int n = in.nextInt();
-        char ch[] = in.next().toCharArray();
-        Arrays.sort(ch);
-        for(char c:ch){
-            pw.print(c);
+
+        int t = in.nextInt();
+        for (int tt = 0; tt < t; tt++) {
+            int n = in.nextInt();
+            int cnt = Integer.bitCount(n);
+           // debug(Integer.bitCount(n));
+            pw.println((long)Math.pow(2,cnt));
         }
         pw.close();
     }
