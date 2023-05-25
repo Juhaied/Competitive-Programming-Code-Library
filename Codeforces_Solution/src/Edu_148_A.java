5ec05f088a84b32109935777e10a0233f0e99990
Juhaied <juhaied.hossen@northsouth.edu>
2023-05-25 22:51:08 +0600

    Live Contest Solved Educational round 148

--- Codeforces_Solution/src/Edu_148_A.java
+++ Codeforces_Solution/src/Edu_148_A.java
@@ -1,43 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.PrintWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Edu_148_A {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
+        int test = in.nextInt();
+        for(int t = 1;t<=test;t++){
+            int n = in.nextInt(), x = in.nextInt();
 
-        int t = in.nextInt();
-        for (int tt = 0; tt < t; tt++) {
-            String s = in.next();
-
-            if(s.length()==2){
-                pw.println("NO");
+            if(n %x !=0){
+                pw.println(1);
+                pw.println(n);
             }
             else {
-                HashSet<Character> set = new HashSet<>();
-             //   HashMap<Character,Integer> map = new HashMap<>();
-
-                for(int i = 0;i<s.length()/2;i++){
-                    char ch = s.charAt(i);
-
-                    set.add(ch);
-                }
-
-                if(set.size()==1){
-                    pw.println("NO");
-                }
-                else {
-                    pw.println("YES");
-                }
-
-
+                pw.println(2);
+                pw.println(n-1+" "+(1));
             }
-
         }
         pw.close();
+
     }
 
-    static void debug(Object... obj) {
+    static  void debug(Object...obj){
         System.err.println(Arrays.deepToString(obj));
     }
 }
--- Codeforces_Solution/src/Edu_148_B.java
+++ Codeforces_Solution/src/Edu_148_B.java
@@ -1,51 +1,41 @@
-import java.io.*;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Edu_148_B {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
+        int test = in.nextInt();
+        for(int t = 1;t<=test;t++){
+            int n = in.nextInt(),cnt  = 0;
+            String s = in.next();
+            int c1 = 0,c2 = 0;
+            int a1  = -1,a2 = -1;
+            for(int i = 0;i<s.length();i++){
+                if(s.charAt(i)=='<'){
+                    c1++;
 
-        int t = in.nextInt();
-        for (int tt = 0; tt < t; tt++) {
-            int n = in.nextInt();
-            int a[] = new int[n];
-
-            HashSet<Integer> set = new HashSet<>();
-            for(int i = 0;i<n;i++){
-                a[i] = in.nextInt();
-            }
-
-            n = removeDuplicates(a,n);
-            int ans  = n;
-
-            //debug(ans);
-
-            for(int i = 0;i+2<n;++i){
-                if(a[i]>a[i+1] && a[i+1]>a[i+2]){
-                    ans--;
+                    a2 = Math.max(a2,c2);
+                    c2 = 0;
                 }
-                if(a[i]<a[i+1] && a[i+1]<a[i+2]){
-                    ans--;
+                else {
+                    c2++;
+                    a1 = Math.max(a1,c1);
+                    c1=0;
                 }
             }
 
-            pw.println(ans);
+            int max= Math.max(c1,c2);
 
+            max= Math.max(max,Math.max(a1,a2));
+            pw.println(max+1);
         }
         pw.close();
-    }
-    public static int removeDuplicates(int[] nums, int n) {
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (i == n - 1 || nums[i] != nums[i + 1]) {
-                nums[j++] = nums[i];
-            }
-        }
-        return j;
+
     }
 
-    static void debug(Object... obj) {
+    static  void debug(Object...obj){
         System.err.println(Arrays.deepToString(obj));
     }
 }
