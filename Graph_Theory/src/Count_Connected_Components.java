31efad68ddbbceb8900c521176bbeee7d1c6c454
Juhaied <juhaied.hossen@northsouth.edu>
2023-06-02 21:17:07 +0600

    Graph : https://atcoder.jp/contests/abc284/tasks/abc284_c

---
+++ Graph_Theory/src/Count_Connected_Components.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Count_Connected_Components {
+    static ArrayList<Integer> adj[];
+    static boolean visited[];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int n = in.nextInt(), m = in.nextInt();
+        adj = new ArrayList[n+1];
+        visited = new boolean[n+1];
+
+        for (int i = 0;i<=n;i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0;i<m;i++){
+            int u = in.nextInt(), v =in.nextInt();
+
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+        int cnt  = 0;
+
+        for (int i =1;i<=n;i++){
+            if(!visited[i]){
+                dfs(i);
+                cnt++;
+            }
+        }
+        pw.println(cnt);
+        pw.close();
+    }
+
+    public static void dfs(int u) {
+        visited[u] = true;
+
+        for (int v: adj[u]) {
+            if (!visited[v]) {
+                dfs(v);
+            }
+        }
+    }
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj));
+    }
+}
