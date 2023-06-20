89e71f9ac0d92978c57e705a8007a6702bf75df3
Juhaied <juhaied.hossen@northsouth.edu>
2023-06-20 10:24:05 +0600

    Codeforces : https://codeforces.com/contest/1454/problem/D

---
+++ Codeforces_Solution/src/Div3_683_D.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Div3_683_D {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int t = in.nextInt();
+        for (int tt = 0; tt < t; tt++) {
+
+            long n = in.nextLong();
+            long temp = n;
+            long maxCount = 0;
+            long maxInt = 0;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                int count = 0;
+                while (n % i == 0) {
+                    count++;
+                    n /= i;
+                }
+                if (maxCount < count) {
+                    maxCount = count;
+                    maxInt = i;
+                }
+            }
+            // debug(maxCount+" "+maxInt);
+            if (maxInt == 0) {
+                pw.println(1);
+                pw.println(temp);
+            } else {
+                pw.println(maxCount);
+                while (temp % (maxInt * maxInt) == 0) {
+                    pw.print(maxInt + " ");
+                    temp = temp / maxInt;
+                }
+                pw.println(temp);
+            }
+
+        }
+        pw.close();
+    }
+
+    static void debug(Object... obj) {
+        System.err.println(Arrays.deepToString(obj));
+    }
+}
