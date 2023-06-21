0b1271f87c83e0ad7b2144dda6424de6e16ffdaa
Juhaied <juhaied.hossen@northsouth.edu>
2023-06-21 19:10:47 +0600

    Long Contest : https://vjudge.net/contest/563116#problem/C

---
+++ Number_Theory/src/Long_Contest_Counting_Divisors.java
@@ -0,0 +1,154 @@
+//Long_Contest_Counting_Divisors
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Long_Contest_Counting_Divisors {
+
+    static int MAX = 1000000+5;
+    static int NOD[] =  new int[MAX];
+    public static void main(String[] args) {
+        FastReader in = new FastReader(System.in);
+
+        NOD_Func();
+
+        int test = in.nextInt();
+        for(int i = 1;i<=test;i++) {
+            System.out.println(NOD[in.nextInt()]);
+        }
+    }
+
+
+    static void NOD_Func()
+    {
+        for(int i = 1;i<MAX;i++) {
+            for(int j = i;j<MAX;j+=i) {
+                NOD[j]++;
+            }
+        }
+    }
+
+
+    static void debug(Object...obj){
+        System.err.println(Arrays.deepToString(obj));
+    }
+    static class FastReader {
+        InputStream is;
+        private byte[] inbuf = new byte[1024];
+        private int lenbuf = 0, ptrbuf = 0;
+
+        public FastReader(InputStream is) {
+            this.is = is;
+        }
+
+        public int readByte() {
+            if (lenbuf == -1) throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        public boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private boolean isEndOfLine(int c) {
+            return c == '\n' || c == '\r' || c == -1;
+        }
+
+        public int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b)) ;
+            return b;
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+
+        public String nextLine() {
+            int c = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!isEndOfLine(c)) {
+                sb.appendCodePoint(c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = (num << 3) + (num << 1) + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = (num << 3) + (num << 1) + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public char[] next(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public char readChar() {
+            return (char) skip();
+        }
+    }
+
+
+}
