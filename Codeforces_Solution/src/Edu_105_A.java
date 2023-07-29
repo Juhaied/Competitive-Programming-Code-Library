3f3e922b30c37db9bd62bca39e329f06b13b3d3f
Juhaied <juhaied.hossen@northsouth.edu>
2023-07-29 11:57:18 +0600

    7.29.23 - 900 rating

--- Codeforces_Solution/src/Edu_105_A.java
+++ Codeforces_Solution/src/Edu_105_A.java
@@ -1,22 +1,15 @@
 import java.io.*;
 import java.util.*;
 
-public class Div2_876_A {
+public class Edu_105_A {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
         int t = in.nextInt();
         for (int tt = 0; tt < t; tt++) {
-            int n = in.nextInt(), k = in.nextInt();
 
-            int ans = (n-1)/k;
 
-            if((n-1) % k !=0){
-                ans++;
-            }
-            ans++;
-            pw.println(ans);
         }
         pw.close();
     }
