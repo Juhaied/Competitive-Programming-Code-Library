ddf0a69c4aa5ef0237349d7a7c0c566b41a80f4d
Juhaied <juhaied.hossen@northsouth.edu>
2023-07-31 21:19:39 +0600

    Bit Counting of a number in two ways

--- BitMasking/src/Count_Bits.java
+++ BitMasking/src/Count_Bits.java
@@ -6,14 +6,29 @@ public class Count_Bits {
         Scanner in = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
-        int n = 15;
+        int n = in.nextInt();
+        pw.println(count_bit(n));
+        pw.println(count_bit_hacks(n));
+        pw.close();
+    }
+    static int count_bit(int n){
         int cnt = 0;
         while (n>0){
             cnt+=(n&1);
             n =n>>1;
         }
-        pw.println(cnt);
-        pw.close();
+        return cnt;
+    }
+
+    static int count_bit_hacks(int n){
+        int cnt  = 0;
+
+        while (n>0){
+            n = n &(n-1);
+            cnt++;
+        }
+
+        return cnt;
     }
 
     static void debug(Object... obj) {
